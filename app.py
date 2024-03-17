013100b89268e2288d54fb958599d97b3f13f105
Chun Wu <the.chun.wu@gmail.com>
2024-03-16 20:53:04 -0700

    Configured basic error handling...

--- app.py
+++ app.py
@@ -7,9 +7,22 @@ app = Flask(__name__)
 # Create a route decorator
 @app.route("/")
 def main():
-    return render_template("index.html")
+    last_name = "Wu"
+    return render_template("index.html", last_name=last_name)
 
 
 @app.route("/user/<string:name>")
 def user(name):
-    return "<h1>Hello {}!</h1>".format(name)
+    return render_template("user.html", name=name)
+
+
+# Invalid URL
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+
+# Internal Server Error
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 500
