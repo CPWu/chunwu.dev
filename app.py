55a7d675282a59dcf67733b0e2ffd85b8e2bd7ae
Chun Wu <the.chun.wu@gmail.com>
2024-03-16 22:37:49 -0700

    Added a HTML Form

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 # Create a Flask Instance
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "Password123!"
 
 
 # Create a route decorator
@@ -26,3 +30,20 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template("500.html"), 500
+
+
+# Create a Form Class
+class NamerForm(FlaskForm):
+    name = StringField("What's your name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+
+# Create name page
+@app.route("/name", methods=["GET", "POST"])
+def name():
+    name = None
+    form = NamerForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.date = ""
+    return render_template("name.html", name=name, form=form)
